9e0909341268fdd1129207ad57814587032970e4
Michael Chudinov <mikael@chudinov.net>
2026-02-09 00:52:55 +0100

    Config dapr ports.

--- PubSubDapr.AppHost/AppHost.cs
+++ PubSubDapr.AppHost/AppHost.cs
@@ -11,14 +11,22 @@ topic.AddServiceBusSubscription("subscription");
 var dapr = builder.AddDapr();
 
 builder.AddProject<Projects.Pub>("pub")
-    .WithDaprSidecar(new DaprSidecarOptions
-    {
-        AppId = "publisher",
-        Config = ".\\.dapr\\components",
-        DaprGrpcPort = 59004,
-        DaprHttpPort = 59005,
-        LogLevel = "Debug"
-    })
+           //.WithDaprSidecar(new DaprSidecarOptions
+           //{
+           //    AppId = "publisher",
+           //    Config = ".\\.dapr\\components",
+           //    DaprGrpcPort = 59004,
+           //    DaprHttpPort = 59005,
+           //    LogLevel = "Debug"
+           //})
+           .WithDaprSidecar(new DaprSidecarOptions
+           {
+               Config = ".\\.dapr\\components",
+               AppId = "my-app-id",
+               DaprGrpcPort = 50001,
+               DaprHttpPort = 3500,
+               MetricsPort = 9090
+           })
     .WithEnvironment("AzureServiceBus__ConnectionString", "Endpoint=sb://localhost;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;")
     .WaitFor(serviceBus);
 
