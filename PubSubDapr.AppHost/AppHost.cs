0478ee567ad3e1053a822bf847425095acec10d8
Michael Chudinov <mikael@chudinov.net>
2026-03-20 00:27:30 +0100

    Fix pubsub.

--- PubSubDapr.AppHost/AppHost.cs
+++ PubSubDapr.AppHost/AppHost.cs
@@ -11,33 +11,20 @@ topic.AddServiceBusSubscription("subscription");
 var daprPath = Path.Combine(Directory.GetCurrentDirectory(), ".dapr\\components");
 var daprConfigPath = Path.Combine(daprPath, "global.yaml");
 
-//var dapr = builder.AddDapr();
-
 builder.AddProject<Projects.Pub>("pub")
-           //.WithDaprSidecar(new DaprSidecarOptions
-           //{
-           //    AppId = "publisher",
-           //    Config = ".\\.dapr\\components",
-           //    DaprGrpcPort = 59004,
-           //    DaprHttpPort = 59005,
-           //    LogLevel = "Debug"
-           //})
-           .WithDaprSidecar(new DaprSidecarOptions
-           {
-               //Config = ".\\.dapr\\config.yaml", // Path to your Dapr config file (if any)
-               //ComponentsPath = ".\\.dapr\\components", // Path to your components directory
-               //Config = ".\\.dapr\\components",
-               AppId = "pub-app-id",
-               AppPort = 8082,
-               DaprGrpcPort = 50001,
-               DaprHttpPort = 3500,
-               MetricsPort = 9090,
-               ResourcesPaths = [daprPath], 
-               LogLevel = "Debug",
-               Config = daprConfigPath,
-               EnableApiLogging = true
-           })
-    .WithEnvironment("AzureServiceBus__ConnectionString", "Endpoint=sb://localhost;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;")
+    .WithEnvironment("AzureServiceBus__ConnectionString", serviceBus.Resource.ConnectionStringExpression)
+    .WithDaprSidecar(new DaprSidecarOptions
+    {
+        AppId = "pub-app-id",
+        AppPort = 8082,
+        DaprGrpcPort = 50001,
+        DaprHttpPort = 3500,
+        MetricsPort = 9090,
+        ResourcesPaths = [daprPath], 
+        LogLevel = "Debug",
+        Config = daprConfigPath,
+        EnableApiLogging = true
+    })    
     .WaitFor(serviceBus);
 
 builder.Build().Run();
